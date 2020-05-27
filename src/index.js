e80c565fd9cdb7eb5f6f91e2771aff6dbc186738
makombekepha <makombekepha@gmail.com>
2020-05-27 17:10:23 +0300

    react ecommerce deploy

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import {ProductProvider} from './Context';
 import * as serviceWorker from './serviceWorker';
+import Product from './components/Product';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
